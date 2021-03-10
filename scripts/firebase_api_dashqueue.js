c571945d0f551f4d6897f8ea93742d30e5c4c4c7
Austin He <ahe8@my.bcit.ca>
2021-03-10 11:16:48 -0800

    Added firebase api script

---
+++ scripts/firebase_api_dashqueue.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBlVVWzzlSj2gZaqjwRfJ0hjFf_agCiIwU",
+    authDomain: "dashqueue-ad3d8.firebaseapp.com",
+    projectId: "dashqueue-ad3d8",
+    storageBucket: "dashqueue-ad3d8.appspot.com",
+    messagingSenderId: "1052516689603",
+    appId: "1:1052516689603:web:b36b21b29d5cb2a45aa476"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
