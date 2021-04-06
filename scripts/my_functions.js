8f387fb362afa2a2145e778f113051cb225b955a
Max Anderson-Baier <77411077+MaxAndersonBaier@users.noreply.github.com>
2021-04-06 14:13:50 -0700

    adding functionality to search bar

--- scripts/my_functions.js
+++ scripts/my_functions.js
@@ -10,9 +10,22 @@ function getStore() {
             .then(function (snap) {
                 snap.forEach(function(doc) {
                     console.log(doc.data());
+                    document.getElementById("availablestores").innerHTML = doc.data().location
                     //do something with the data
                 })
             })
     })
 }
 getStore();
+
+
+
+function readQuote() {
+    db.collection("shopping").doc("popular_stores")
+        .onSnapshot(
+            function (snap) {
+                console.log(snap.data());
+                document.getElementById("store-goes-here").innerHTML = snap.data().store;
+            })
+}
+readQuote();
