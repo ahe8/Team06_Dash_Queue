0a5a7f7a0a346768c6b34baf85e24c7d6e241e8c
Max Anderson-Baier <77411077+MaxAndersonBaier@users.noreply.github.com>
2021-04-06 14:41:42 -0700

    database can now be queried!

--- scripts/my_functions.js
+++ scripts/my_functions.js
@@ -3,18 +3,17 @@ function getStore() {
         var location = document.getElementById("store").value;
         console.log(location);
 
-				//read grocery store collection from firestore, with query
+        //read grocery store collection from firestore, with query
         db.collection("Grocery Stores")
             .where("name", "==", location)
             .get()
             .then(function (snap) {
-                snap.forEach(function(doc) {
+                snap.forEach(function (doc) {
                     console.log(doc.data());
-                    document.getElementById("availablestores1").innerHTML = doc.data().name;
-                    document.getElementById("availablelocations1").innerHTML = doc.data().location;
-                    document.getElementById("availablestores2").innerHTML = doc.data().name; 
-                    document.getElementById("availablelocations2").innerHTML = doc.data().location;
-                    //do something with the data
+                    var name = doc.data().name;
+                    var location = doc.data().location;
+                    $("#details-go-here").append("<h1> " + name + "</h1>");
+                    $("#details-go-here").append("<h1> " + location + "</h1>");
                 })
             })
     })
@@ -23,12 +22,38 @@ getStore();
 
 
 
-function readQuote() {
-    db.collection("shopping").doc("popular_stores")
-        .onSnapshot(
-            function (snap) {
-                console.log(snap.data());
-                document.getElementById("store-goes-here").innerHTML = snap.data().store;
-            })
-}
-readQuote();
+
+// db.collection("Grocery Stores")
+//     .doc(id)
+//     .get()
+//     .then(function (doc) {   // display details!
+//         var name = doc.data().fields.name;
+//         var geo_area = doc.data().fields.geo_local_area;
+//         var coord = doc.data().fields.geom.coordinates;
+//         var url = doc.data().fields.url;
+//         $("#details-go-here").append("<h1> " + name + "</h1>");
+//         $("#details-go-here").append("<h1> " + geo_area + "</h1>");
+//         $("#details-go-here").append("<h1> " + coord + "</h1>");
+//         $("#details-go-here").append("<a href='" + url + "' > " + url + ">");
+//         var likeid = "like" + id;
+//         $("#details-go-here").append("<h1 id='" + likeid + "'> CLICK HERE TO LIKE </h1>");
+//         addLikeListener(id, likeid);
+//         showLikes(likeid);
+//     })
+// }
+// getDetails();
+
+
+
+
+
+
+// function readQuote() {
+//     db.collection("shopping").doc("popular_stores")
+//         .onSnapshot(
+//             function (snap) {
+//                 console.log(snap.data());
+//                 document.getElementById("store-goes-here").innerHTML = snap.data().store;
+//             })
+// }
+// readQuote();
