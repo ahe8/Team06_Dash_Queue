605aeb6e369dd0642f528518bb0a200abae302fb
Austin He <ahe8@my.bcit.ca>
2021-04-12 10:12:02 -0700

    add datetime for scheduling

--- scripts/my_functions.js
+++ scripts/my_functions.js
@@ -1,23 +1,56 @@
 function getStore() {
     document.getElementById("submit").addEventListener('click', function () {
-        var location = document.getElementById("store").value;
-        console.log(location);
+        var store = document.getElementById("store").value;
+        console.log(store);
 
-        //read grocery store collection from firestore, with query
-        db.collection("Grocery Stores")
-            .where("name", "==", location)
+				//read cities collection from firestore, with query
+        db.collection("stores")
+            .where("company_name", "==", store)
             .get()
             .then(function (snap) {
-                snap.forEach(function (doc) {
+                snap.forEach(function(doc) {
                     console.log(doc.data());
-                    var name = doc.data().name;
-                    var location = doc.data().location;
-                    var time = doc.data().time;
+                    var name = doc.data().company_name;
+                    var address = doc.data().address;
+                    var city = doc.data().city;
+                    var postalcode = doc.data().postalcode;
+                    var province = doc.data().province;
+                    var mintime = doc.data().openingtime;
+                    var maxtime = doc.data().closingtime;
+
+                    var today = new Date();
+                    var dd = today.getDate();
+                    var mm = today.getMonth()+1; //January is 0!
+                    var yyyy = today.getFullYear();
+                    var currenthour = today.getHours()
+                    var currentminute = today.getMinutes();
+                    var currenttime = currenthour+":"+currentminute;
+                    console.log(currenttime)
+
+                    if(dd<10){
+                            dd='0'+dd
+                        } 
+                        if(mm<10){
+                            mm='0'+mm
+                        } 
+
+                    if(mintime<=currenttime){
+                        mintime = currenttime;
+                    }
+
+                    today = yyyy+'-'+mm+'-'+dd;
+                    
                     $("#details-go-here").append("<h1> " + name + "</h1>");
-                    $("#details-go-here").append("<li> Location: " + location + "</li>");
-                    $("#details-go-here").append("Available Appointments: <a href= confrimationpage.html>" + time + "</a>");
+                    $("#details-go-here").append("<li> Address: " + address + "</li>");
+                    $("#details-go-here").append("<li> City: " + city + "</li>");
+                    $("#details-go-here").append("<li> Postal Code: " + postalcode + "</li>");
+                    $("#details-go-here").append("<li> Province: " + province + "</li><br>");
+                    $("#details-go-here").append("<label for='timepicker'>Select a time (date and time):</label>");
+                    $("#details-go-here").append("<input type='date' min='" + today + "'><input type='time' min='"+mintime+"' max ='"+maxtime+"'><br><br>");
+                    $("#details-go-here").append("<a href='/confirmationpage.html'>Next</a>");
                 })
             })
     })
 }
+
 getStore();
